8746b4fa01dd039faed1510adcbd4641208d9d96
parvajio <mdparvajmosharof17@gmail.com>
2025-04-08 10:37:41 +0600

    upload route

---
+++ app/upload/page.tsx
@@ -0,0 +1,12 @@
+import FileUpload from "@/components/FileUpload";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[70vh]">
+      <FileUpload></FileUpload>
+    </div>
+  );
+};
+
+export default page;
